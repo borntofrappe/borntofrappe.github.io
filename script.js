4fcc6a83fefa66aeed7d89c754ea06d1075b3200
gabrielecorti <borntofrappe@gmail.com>
2018-10-20 23:08:17 +0200

    update texture, change color depending on hour

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 /** script logic
- * change the accent color on load
- * change the job description on load
+ * change the accent color depending on the time of day
+ * change the job description at random
  */
 // create a function which accepts an array as argument and returns a random item from the same
 function randomItem(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
+
 // COLOR ACCENT
-const accent = [
-  '#47ffb1',
-  '#ff6347',
-  '#245DCC',
-  '#CDFF60'
-];
-// select the root element and update the --color-accent property with a hue picked at random
-document.documentElement.style.setProperty('--color-accent', randomItem(accent));
+// compute the color accent choosing a selected hue (although a random color may be picked)
+const hue = 9;
+// create an instance of the date object
+const date = new Date();
+// retrieve the hours
+// timeOfDay [0-23]
+const timeOfDay = date.getHours();
+// adjustedTOD mapping hours after or equal to 7 at the beginning of the range
+// pushing the hours prior 7 at the very end
+// meaning 7 becomes 0, 23 becomes 16
+// 0 becomes 17 (0+23-6), 6 becomes 23 (6+23-6)
+const adjustedTOD = (timeOfDay >= 7) ? timeOfDay - 7 : timeOfDay + (23 - 6);
+// normalizedTOD [0-1]
+const normalizedTOD = (adjustedTOD / 23).toFixed(2);
+// saturation [40-100]
+// lightness [40-60]
+// considering the range the other way around (0 -> 1, 1 -> 0)
+const saturation = (40 + ((100 - 40)) * (1 - normalizedTOD));
+const lightness = (40 + ((60 - 40)) * (1 - normalizedTOD));
+
+// select the root element and update the --color-accent property with the described hsl color
+document.documentElement.style.setProperty('--color-accent', `hsl(${hue}, ${saturation}%, ${lightness}%)`);
 
 // JOB DESCRIPTION
 // detail a series of catchy expression
