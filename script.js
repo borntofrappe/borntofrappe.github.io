d6e13495aee10489df4bdf811adb95984a6e9266
gabrielecorti <borntofrappe@gmail.com>
2018-10-23 17:36:53 +0200

    add the project's functionality

--- script.js
+++ script.js
@@ -7,10 +7,11 @@
 
 /* utility functions
 - a function returning a random item from an array
+- a function returning a random number between a min and max optional values
 */
-function randomItem(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
+const randomItem = array => array[Math.floor(Math.random() * array.length)];
+const randomInteger = (min = 0, max = 100) => Math.floor(Math.random() * (max - min) + min);
+
 
 // ACCENT COLOR
 // compute the color accent from the selected hue
@@ -51,9 +52,117 @@ const jobDescription = document.querySelector('i');
 // change the text of the <i> element from one of the expressions at random
 jobDescription.textContent = randomItem(job);
 
+// PROJECT
+// describe the projects in an array
+const projects = [
+  /* each object including
+  - title
+  - description
+  - stack (an array of strings describing the technology stack)
+  - url forwarding toward the project
+  */
+  {
+    title: 'Teaching Dashboard',
+    description: 'An appealing visual summarising valuable information on JavaScript methods.',
+    stack: [
+      'html',
+      'css',
+      'js',
+      'react'
+    ],
+    url: 'https://codepen.io/borntofrappe/full/KxdOVo'
+  },
+  {
+    title: 'Signature Generator',
+    description: 'An intriguing substite to hand-written signatures.',
+    stack: [
+      'html',
+      'css',
+      'js',
+      'svg'
+    ],
+    url: 'https://codepen.io/borntofrappe/full/WyVZVp'
+  },
+  {
+    title: 'CSS Quiz',
+    description: 'A multiple choice game to practice with CSS.',
+    stack: [
+      'html',
+      'css',
+      'js'
+    ],
+    url: 'https://codepen.io/borntofrappe/full/OZaOWo'
+  },
+  {
+    title: 'Day Picker',
+    description: 'No longer wondering which day it was. Or it will be.',
+    stack: [
+      'html',
+      'css',
+      'js',
+      'node',
+      'pug'
+    ],
+    url: 'https://witty-recess.glitch.me/'
+  }
+];
+// create a function creating a new value for the border radius property
+const borderRadius = () => {
+  // store 8 random percentages in an array
+  const randomNumbers = [];
+  for (let i = 0; i < 8; i += 1) {
+    randomNumbers.push(`${randomInteger()}%`);
+  }
+  // return a string which includes a forward slash after the 4th item
+  return [...randomNumbers.slice(0, 4), '/', ...randomNumbers.slice(4)].join(' ');
+};
+
+// create a function fabricating the markup for each project
+const projectMarkup = project => `
+<h2>${project.title}</h2>
+<p>${project.description}</p>
+<ul>
+${project.stack.map(stack => `
+  <li>
+    <svg>
+      <use href="#${stack}-icon"/>
+    </svg>
+  </li>
+`).join('')}
+</ul>
+`;
+
+// create a function to populate the project section
+const addProject = (project) => {
+  // change the border-radius property of the project icon
+  const projectIcon = document.querySelector('.project__icon');
+  projectIcon.style.borderRadius = borderRadius();
+  // target the article
+  const projectInfo = document.querySelector('.project--info');
+  // add the selected project
+  projectInfo.innerHTML = projectMarkup(project);
+
+  // target the anchor link element and add the URL
+  const projectLink = document.querySelector('.project--controls a');
+  projectLink.setAttribute('href', project.url);
+};
+
+addProject(randomItem(projects));
+
+// add an event listener on the button element
+const projectButton = document.querySelector('.project--controls button');
+// on click call the function to add a new project
+projectButton.addEventListener('click', () => addProject(randomItem(projects)));
+
 // SOCIAL ICONS
-// describe each icon and the matching text
+// describe the social links in an array
 const social = [
+  /* each object including
+  - link, to match the anchor link with a data-link attribute
+  - url, forwarding toward the profile
+  - copy, describing a brief description for each profile
+  - color, match each icons' color
+  */
   {
     link: 'twitter',
     url: 'https://twitter.com/borntofrappe',
@@ -121,3 +230,4 @@ function paint(event) {
 
 // trigger the function whenever hovering on one of the selected anchor links
 socialLinks.forEach(socialLink => socialLink.addEventListener('mouseenter', paint));
+socialLinks.forEach(socialLink => socialLink.addEventListener('focus', paint));
