3529699896e64fc838c9eed4109dcd02e0ea027f
borntofrappe <borntofrappe@gmail.com>
2021-09-04 11:16:59 +0200

    spritesheet demo

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ A page to tinker with Github pages and a pixelated look.
 
 ## demo
 
-In the `demo` folder I created a small playground with Lua and Love2D, so to showcase what I want to achieve with the spritesheet.
+In the `demo` folder I created a small playground with Lua and Love2D, so to showcase what I want to achieve with the spritesheet. The idea is to repeat a tile, the 4 by 4 square with a light gree color, in the background, and then render the character, horizon line and field at the bottom of the window. Following a specific action, which in the demo means pressing the `down` arrow key, the idea is to then animate the player through its four possible frames, the field in its `y` coordinate.
+
+The demo includes a few Love2D specifc features, but understanding their purpose is more important than learning how the functions work. The quads help to section the spritesheet into its multiple components: player, horizon line, field and background tile. The sprite batch helps to efficiently draw a series of visuals, especially those visuals which do not change over time like the tiles in the background and the repeated horizon line. The stencil helps to hide the field as it moves above the horizon line.
+
+In the website, the player and field could be updated with an animation, on a loop, but also following a specific action. Consider once more a key press, or again the scroll of the window. In this last instance you would fix the visual in the bottom portion and limit the content to the area above.
--- demo/main.lua
+++ demo/main.lua
@@ -11,15 +11,22 @@ local HORIZON_HEIGHT = 8
 local FIELD_WIDTH = 64
 local FIELD_HEIGHT = 88
 
+local HORIZON_Y = HEIGHT - FIELD_HEIGHT - HORIZON_HEIGHT
+
+local UPDATE_TIMER = 0.15
+
 local function GenerateQuadsCharacter(atlas)
     local quads = {}
 
-    local frames = math.floor(atlas:getWidth() / 64)
+    local frames = math.floor(atlas:getWidth() / CHARACTER_SIZE)
 
-    for i = 1, frames do 
-        table.insert(quads, love.graphics.newQuad(CHARACTER_SIZE * (i - 1), 0, CHARACTER_SIZE, CHARACTER_SIZE, atlas:getDimensions()))
+    for i = 1, frames do
+        table.insert(
+            quads,
+            love.graphics.newQuad(CHARACTER_SIZE * (i - 1), 0, CHARACTER_SIZE, CHARACTER_SIZE, atlas:getDimensions())
+        )
     end
-    
+
     return quads
 end
 
@@ -36,33 +43,102 @@ local function GenerateQuadBackground(atlas)
 end
 
 function love.load()
-    love.window.setTitle('Spritesheet demo')
+    love.window.setTitle("Spritesheet demo")
     love.window.setMode(WIDTH, HEIGHT)
-    love.graphics.setBackgroundColor(0.62, 0.7,0.62)
+    love.graphics.setBackgroundColor(0.62, 0.7, 0.62)
 
-    gTexture = love.graphics.newImage('spritesheet.png')
+    gTexture = love.graphics.newImage("spritesheet.png")
     gQuads = {
         ["character"] = GenerateQuadsCharacter(gTexture),
         ["horizon"] = GenerateQuadHorizon(gTexture),
         ["field"] = GenerateQuadField(gTexture),
-        ["background"] = GenerateQuadBackground(gTexture),
+        ["background"] = GenerateQuadBackground(gTexture)
     }
+
+    gSpriteBatch = love.graphics.newSpriteBatch(gTexture)
+    for column = 1, COLUMNS do
+        for row = 1, ROWS do
+            gSpriteBatch:add(gQuads.background, (column - 1) * TILE_SIZE, (row - 1) * TILE_SIZE)
+        end
+    end
+
+    local xHorizon = 0
+    repeat
+        gSpriteBatch:add(gQuads.horizon, xHorizon, HORIZON_Y)
+        xHorizon = xHorizon + HORIZON_WIDTH
+    until xHorizon > WIDTH
+
+    gCharacter = {
+        ["x"] = TILE_SIZE * 2,
+        ["y"] = HORIZON_Y - CHARACTER_SIZE,
+        ["frame"] = 1
+    }
+
+    gField = {
+        ["x"] = {},
+        ["y"] = HORIZON_Y + HORIZON_HEIGHT,
+        ["offsetY"] = 0
+    }
+
+    local xField = 0
+    repeat
+        table.insert(gField.x, xField)
+        xField = xField + FIELD_WIDTH
+    until xField > WIDTH
+
+    gTimer = 0
 end
 
 function love.keypressed(key)
-    if key == 'escape' then 
+    if key == "escape" then
         love.event.quit()
+    end
+
+    if key == "down" then
+        gCharacter.frame = 1
     end
 end
 
-function love.update()
+function love.keyreleased(key)
+    if key == "down" then
+        gCharacter.frame = 1
+    end
+end
 
+function love.update(dt)
+    if love.keyboard.isDown("down") then
+        gTimer = gTimer + dt
+        if gTimer >= UPDATE_TIMER then
+            gTimer = gTimer % UPDATE_TIMER
+            gCharacter.frame = gCharacter.frame == #gQuads.character and 1 or gCharacter.frame + 1
+
+            gField.offsetY = gField.offsetY == FIELD_HEIGHT - TILE_SIZE and 0 or gField.offsetY + TILE_SIZE
+        end
+    end
+end
+
+local function stencilFunction()
+    love.graphics.rectangle("fill", 0, gField.y, WIDTH, FIELD_HEIGHT)
 end
 
 function love.draw()
-    love.graphics.setColor(1,1,1)
-    love.graphics.draw(gTexture, gQuads.character[1], 0, 0)
-    love.graphics.draw(gTexture, gQuads.horizon, 0, 64)
-    love.graphics.draw(gTexture, gQuads.field, 0, 72)
-end
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(gSpriteBatch, 0, 0)
 
+    love.graphics.draw(gTexture, gQuads.character[gCharacter.frame], gCharacter.x, gCharacter.y)
+
+    love.graphics.stencil(stencilFunction, "replace", 1)
+
+    love.graphics.setStencilTest("greater", 0)
+
+    love.graphics.translate(0, -gField.offsetY)
+    for i, x in ipairs(gField.x) do
+        love.graphics.draw(gTexture, gQuads.field, x, gField.y)
+    end
+    love.graphics.translate(0, FIELD_HEIGHT)
+    for i, x in ipairs(gField.x) do
+        love.graphics.draw(gTexture, gQuads.field, x, gField.y)
+    end
+
+    love.graphics.setStencilTest()
+end
