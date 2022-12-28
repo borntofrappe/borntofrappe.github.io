a2dd034235bf0b0dc5ecbb91a561544d568538b6
Gabriele Corti <borntofrappe@gmail.com>
2022-12-28 10:49:00 +0100

    update github pages config

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null,
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: dev ? '' : '/borntofrappe.github.io'
-		},
-		appDir: 'internal'
+		adapter: adapter()
 	}
 };
 
