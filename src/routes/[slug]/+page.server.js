028d39eeecb5f03e77b275918b0ee164cd49a9f9
Gabriele Corti <borntofrappe@gmail.com>
2022-12-29 16:10:07 +0100

    serve blog post

---
+++ src/routes/[slug]/+page.server.js
@@ -0,0 +1,33 @@
+import markdown from 'markdown-it';
+import { readFile } from 'node:fs/promises';
+import matter from 'gray-matter';
+import { error } from '@sveltejs/kit';
+import { existsSync } from 'node:fs';
+
+export async function load({ params }) {
+	const { slug } = params;
+
+	const path = `src/log/${slug}.md`;
+
+	if (!existsSync(path)) {
+		throw error(404, 'Log entry not found');
+	}
+
+	try {
+		const file = await readFile(path, { encoding: 'utf-8' });
+
+		const { data, content } = matter(file);
+
+		const { title, entry } = data;
+
+		const html = markdown().render(content);
+
+		return {
+			title,
+			entry,
+			html
+		};
+	} catch (error) {
+		throw new Error(`Unable to process log entry - ${slug}`);
+	}
+}
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let data;
+</script>
+
+<svelte:head>
+	<title>Log entry {data.entry}: {data.title}</title>
+</svelte:head>
+
+<main>
+	<h1>{data.title}</h1>
+	{@html data.html}
+</main>
+
+<style>
+	main {
+		max-width: 60ch;
+		margin-inline: auto;
+	}
+
+	main :global(p) {
+		line-height: 1.5;
+	}
+
+	main > :global(* + *) {
+		margin-top: 0.5em;
+	}
+</style>
