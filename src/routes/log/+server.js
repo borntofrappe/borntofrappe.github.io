143b12542f240917896ae77494971ae301a82900
Gabriele Corti <borntofrappe@gmail.com>
2022-12-31 14:07:29 +0100

    add post route

---
+++ src/routes/log/+server.js
@@ -0,0 +1,33 @@
+import { readdir } from 'node:fs/promises';
+import { resolve, extname } from 'node:path';
+import { readFileSync } from 'node:fs';
+
+import matter from 'gray-matter';
+import markdown from 'markdown-it';
+
+import { json } from '@sveltejs/kit';
+
+export async function GET({ url }) {
+	const { origin } = url;
+
+	const files = await readdir(resolve('src/log/'), { encoding: 'utf-8' });
+	const posts = files
+		.filter((file) => extname(file) === '.md')
+		.map((file) => {
+			const { data, content } = matter(readFileSync(`src/log/${file}`));
+			const slug = file.split('.md')[0];
+			const url = `${origin}/${slug}`;
+
+			const html = markdown().render(content);
+
+			return {
+				url,
+				...data,
+				html
+			};
+		});
+
+	return json({
+		posts: [...posts].sort((a, b) => b.entry - a.entry)
+	});
+}
