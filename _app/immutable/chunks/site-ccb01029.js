95263044021c42b3e57894de4109423fe49e7d74
Gabriele Corti <borntofrappe@gmail.com>
2023-01-15 18:48:00 +0100

    Updates

---
+++ _app/immutable/chunks/site-ccb01029.js
@@ -0,0 +1,1 @@
+const e={origin:"https://borntofrappe.github.io",name:"Gabriele's Pixelated Corner",description:"A pixelated, personal corner on the web belonging to an aspiring game developer named Gabriele Corti."};export{e as s};
