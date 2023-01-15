6e9d3ba1e2889a4a9d98e1be5def6c018bc50b59
Gabriele Corti <borntofrappe@gmail.com>
2023-01-15 18:47:38 +0100

    fix heading selection style

--- src/routes/[slug]/Heading.svelte
+++ src/routes/[slug]/Heading.svelte
@@ -2,12 +2,11 @@
 	export let level = 'h1';
 </script>
 
-<svelte:element this={level} style:--inset-size="8px" class="notched-frame heading">
+<svelte:element
+	this={level}
+	style:--inset-size="8px"
+	class="notched-frame"
+	style:text-align="center"
+>
 	<slot />
 </svelte:element>
-
-<style>
-	.heading {
-		text-align: center;
-	}
-</style>
