5dfba919bfdbbccb6077d82dc76988711d1fc04f
Gabriele Corti <borntofrappe@gmail.com>
2023-01-15 21:05:24 +0100

    hello-world entry

---
+++ src/log/hello-world.md
@@ -0,0 +1,9 @@
+---
+title: Hello world
+date: 2023-01-15
+description: First of many entries? Let's be cautious, and just hope it won't be the last.
+---
+
+If you are reading this introductory post, it means this enterprise is already a success. It means I did manage to deploy a static website on [GitHub Pages](https://pages.github.com/). An application built with [SvelteKit](https://kit.svelte.dev/), with a spiffy landing page and a blog.
+
+That's all? In more ways than one, it currently is.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,11 +15,7 @@
 <figure style:margin-top="0" class="notched-frame">
 	<blockquote>
 		<p>Welcome to a pixelated, personal corner on the web.</p>
-		<p>
-			This is where I jot down a note or two as I further my quest as a game developer.
-			<br />
-			And who knows, maybe grow in the process.
-		</p>
+		<p>This is where I jot down a note or two as I further my quest as a game developer.</p>
 	</blockquote>
 	<figcaption>
 		<cite>Gabriele Corti</cite>
@@ -32,7 +28,7 @@
 	<h2 class="visually-hidden">Log</h2>
 
 	<p style:margin-top="0">
-		Hope you won't mind a few rambling entries, cause this log's choke full of them!
+		Hope you won't mind a few rambling entries, cause this log's going to be choke full of them!
 	</p>
 
 	<ul>
