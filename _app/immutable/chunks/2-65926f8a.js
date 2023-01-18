0230191659c7c6052f0c6c4cc8bbc3b2ba49f4ee
Gabriele Corti <borntofrappe@gmail.com>
2023-01-18 22:23:42 +0100

    Updates

---
+++ _app/immutable/chunks/2-65926f8a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_slug_/_layout.svelte-f6fa8aa9.js";const e=!0;export{t as component,e as server};
--- _app/immutable/chunks/3-92e9183d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b0cf5c41.js";const e=!0;export{t as component,e as server};
